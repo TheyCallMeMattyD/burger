35856cd418c0c3b97b18d41a26ae827277b6508d
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-11 20:14:14 -0500

    debugged remove id paramater from insert value since its already autoincremented

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,11 +2,11 @@ USE burgers_db;
 
 ----- Burger Seeds -----
 
-INSERT INTO burgers (id, burger_name)
+INSERT INTO burgers (burger_name)
 VALUES ("Cheeseburger");
 
-INSERT INTO burgers (id, burger_name)
+INSERT INTO burgers (burger_name)
 VALUES ("Royal with Cheese");
 
-INSERT INTO burgers (id, burger_name)
+INSERT INTO burgers (burger_name)
 VALUES ("Big Mac");
