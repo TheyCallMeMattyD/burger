c300d94dde7dc3c45902992b99b5c5447217d92b
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-11 19:15:11 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+----- Burger Seeds -----
+
+INSERT INTO burgers (id, burger_name)
+VALUES (1, "Cheeseburger");
+
+INSERT INTO burgers (id, burger_name)
+VALUES (2, "Royal with Cheese");
+
+INSERT INTO burgers (id, burger_name)
+VALUES (3, "Big Mac");
