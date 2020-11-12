34c4e83f5dc91d1b1db02d6b700e392218c2ae14
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-12 13:49:04 -0500

    add export burgers code

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,10 @@
 // Import the ORM
 const orm = require('../config/orm.js');
+
+
+// Inside burger.js, import orm.js into burger.js
+
+
+// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+
+module.exports = burgers
