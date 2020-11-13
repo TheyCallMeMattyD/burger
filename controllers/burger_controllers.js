a02bbf14230387383ab0886a5c71bb2f280d4973
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-13 16:58:50 -0500

    install routing

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -4,6 +4,61 @@ const router = express.Router();
 
 const burger = require("burgers.js");
 
+// GET route to display ALL burgers
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// POST route to ADD a new burger to the list
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+// PUT route to UPDATE a burger status to devoured
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition from CONTROLLER", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// DELETE route to remove a burger from the list
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
 // Routes
 
 // create router for the app
