86fe691671f817d5a7c529ac7917a50f1f9e8da3
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-13 17:15:25 -0500

    update filepath

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const burger = require("../models/burgers.js");
+const burger = require("./models/burgers.js");
 
 // GET route to display ALL burgers
 router.get("/", function(req, res) {
