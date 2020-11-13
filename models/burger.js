a1978c87a43eb35315aa267f6f63fb7e68bc625e
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-13 16:57:19 -0500

    fleshout burgerjs and update handlebar layout

--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,28 @@
 // Import the ORM
 const orm = require('../config/orm.js');
 
-
-// Inside burger.js, import orm.js into burger.js
-
-
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+const burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
 
 module.exports = burgers
