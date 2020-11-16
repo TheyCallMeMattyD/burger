f7a3e93080a111ffd86d1a6cf1dae54c947a4de4
TheMattyDGWU <themattydgwu@gmail.com>
2020-11-15 22:04:23 -0500

    added Jawsdb maroon

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_MAROON_URL);
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
